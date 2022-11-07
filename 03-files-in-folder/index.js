2c826b440b1ae49a61f519857ec23e910d29533d
queharambe <queharambe@gmail.com>
2022-11-07 22:20:40 +0100

    refactor readdirSync

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 const currentPath = path.join(__dirname, 'secret-folder')
-filenames = fs.readdirSync(currentPath, { withFileTypes: true } );
-  
-console.log("\nSecret folder filenames:");
+let filenames =  [];
 
-filenames.forEach(file => {
-    let fSize;
-    if (file.isFile()) {
+fs.readdir(currentPath, { withFileTypes: true }, (err, files) => {
+    files.forEach(file => {
+        let fSize;
+        if (file.isFile()) {
         fs.stat(path.join(currentPath,file.name), (err, stats) => {
             fSize = stats.size/1024; 
             console.log(file.name.split(".")[0],"-", path.extname(file.name).slice(1), "-", Number(fSize).toFixed(3)+"kb")
     })
     }
+    })
 });
+  
+console.log("\nSecret folder filenames:");
+
+// filenames.forEach(file => {
+//     let fSize;
+//     if (file.isFile()) {
+//         fs.stat(path.join(currentPath,file.name), (err, stats) => {
+//             fSize = stats.size/1024; 
+//             console.log(file.name.split(".")[0],"-", path.extname(file.name).slice(1), "-", Number(fSize).toFixed(3)+"kb")
+//     })
+//     }
+// });
