4292d532a1ea0aad64f3314358265df79ae9a44d
queharambe <queharambe@gmail.com>
2022-11-09 18:02:40 +0100

    refactor

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,28 +2,22 @@ const { mkdir } = require('node:fs/promises');
 const fs = require('fs')
 const { resolve, join } = require('node:path');
 const path = require('path')
-const { copyFile, constants } =  require('node:fs');
+const { copyFile, constants } = require('node:fs');
 const currentPath = path.join(__dirname, 'files')
 const destPath = path.join(__dirname, 'files-copy')
-
-
-async function makeDirectory() {
-    const projectFolder = join(__dirname, 'files-copy');
-    const dirCreation = await mkdir(projectFolder, { recursive: true });
-}
+const fsPromises = fs.promises;
 
 function copyDIR() {
-    makeDirectory()
-    let filenames = []
-    fs.readdir(currentPath, { withFileTypes: false }, (err, files) => {
-        files.forEach(file => {
-            copyFile(`files/${file}`, `files-copy/${file}`, () => {});
+    fs.rm(destPath, { force: true, recursive: true }, (err) => {
+        fs.mkdir(destPath, { recursive: true }, (err) => {
         })
-    } );
-    // filenames.forEach(file => {
-    //     copyFile(`files/${file}`, `files-copy/${file}`, () => {});
-    // })
-    console.log(filenames)
-}
-
+        fsPromises.readdir(currentPath, { withFileTypes: true }).then(files => {
+            files.forEach(file => {
+                const currentFilePath = path.join(__dirname, 'files', file.name);
+                const destFilePath = path.join(__dirname, 'files-copy', file.name);
+                fsPromises.copyFile(currentFilePath, destFilePath);
+            });
+        });
+    });
+};
 copyDIR()
